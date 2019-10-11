d5b048874858c1ab34c26774a927189c823440bb
MCrisantEV <mcrisant@everis.com>
2019-10-11 11:12:59 -0500

    configuracion inicial

---
+++ src/main/java/com/mcrisant/app/McrisantBootCampApplication.java
@@ -0,0 +1,13 @@
+package com.mcrisant.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McrisantBootCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McrisantBootCampApplication.class, args);
+	}
+
+}
